f2464268e63d37d509b08c7dacc7f7c7025d6a03
nachogl1 <ignacio.gonzalez@codurance.com>
2022-05-23 17:31:25 +0100

    Second commit

--- src/main/java/com/katas/katasWithMyoung/KatasWithMyoungApplication.java
+++ src/main/java/com/katas/katasWithMyoung/KatasWithMyoungApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KatasWithMyoungApplication {
-
+//Hello
 	public static void main(String[] args) {
 		SpringApplication.run(KatasWithMyoungApplication.class, args);
 	}
